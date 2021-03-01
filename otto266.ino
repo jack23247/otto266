be32128a364e4c98c2c6f94efe6d57f39836ab40
Jacopo Maltagliati <mailto:35559767+jack23247@users.noreply.github.com>
2021-03-01 23:04:06 +0100

    Major overhaul including but not limited to initial calibration and an hw mechanism to enable or disable data transmission

--- otto266.ino
+++ otto266.ino
@@ -12,60 +12,145 @@
 #define H_BRIDGE_FAULT_2 5
 #define UNKNOWN_ERROR 6
 
+int32_t gFSMCurrentStatus = UNKNOWN;
+int8_t gIndicatorLedState = LOW;
+
+uint8_t pbStreamBuffer[128];
+
+int32_t gPreviousElapsedTimeMs = 0;
+int32_t gInducedDelay = 0;
+
+#define CALIBRATION_ITERS 1000
+#define CALIBRATION_TARGET 20
+#define ENGINEERING_SAFETY_CONSTANT 1
+
 #define BASELINE 0.435
 /* Federica Di Lauro, [06.02.21 15:08]
-   [In reply to Federica Di Lauro]                    \
-   questo è quello di otto, non ricordo cosa venisse \
-  fuori per l'esercizio di matlab                     \
+   [In reply to Federica Di Lauro]
+   questo è quello di otto, non ricordo cosa venisse
+   fuori per l'esercizio di matlab
  **/
 
-const float ssx[] = {0.2, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.4, 0.6, 0.6, 0.6, 0.6, 0.8, 0.8, 0.8, 0.8, 1, 1, 1, 1, 1.2, 1.2, 1.2, 1.2, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.8, 0.6, 0.6, 0.6, 0.6, 0.4, 0.4, 0.4, 0.4, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.19, 0.18, 0.17, 0.16, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.13, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.13, 0.13, 0.13, 0.13, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15
-                    };
-const float sdx[] = {0.2, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.4, 0.6, 0.6, 0.6, 0.6, 0.8, 0.8, 0.8, 0.8, 1, 1, 1, 1, 1.2, 1.2, 1.2, 1.2, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.8, 0.6, 0.6, 0.6, 0.6, 0.4, 0.4, 0.4, 0.4, 0.2, 0.2, 0.2, 0.2, 0.19, 0.18, 0.17, 0.16, 0.15, 0.14, 0.13, 0.12, 0.11, 0.1, 0.1, 0.1, 0.1, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.13, 0.13, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.13, 0.13, 0.14, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.13, 0.12, 0.11, 0.1, 0.09, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15
-                    };
-const uint32_t dataLen = 295;
-uint32_t curData = 0;
-uint8_t buffer[128];
-int32_t previous_tx_millis = 0;        // will store last time LED was updated
-int32_t currentStatus = UNKNOWN;
+// Dummy encoder values
+#define WHEEL_DATA_LEN 295
+const float gLeftWhlFakeData[] = {0.2, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.4, 0.6, 0.6, 0.6, 0.6, 0.8, 0.8, 0.8, 0.8, 1, 1, 1, 1, 1.2, 1.2, 1.2, 1.2, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.8, 0.6, 0.6, 0.6, 0.6, 0.4, 0.4, 0.4, 0.4, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.19, 0.18, 0.17, 0.16, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.13, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.13, 0.13, 0.13, 0.13, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15};
+const float gRightWhlFakeData[] = {0.2, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.4, 0.6, 0.6, 0.6, 0.6, 0.8, 0.8, 0.8, 0.8, 1, 1, 1, 1, 1.2, 1.2, 1.2, 1.2, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.8, 0.6, 0.6, 0.6, 0.6, 0.4, 0.4, 0.4, 0.4, 0.2, 0.2, 0.2, 0.2, 0.19, 0.18, 0.17, 0.16, 0.15, 0.14, 0.13, 0.12, 0.11, 0.1, 0.1, 0.1, 0.1, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.13, 0.13, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.13, 0.13, 0.14, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.13, 0.12, 0.11, 0.1, 0.09, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15};
+uint32_t gFakeDataCurIdx = 0;
 
-inline void MsgSetVelocities(StatusMessage* msg, float left_wheel_vel, float right_wheel_vel) {
+// Set the values to be encoded by protobuf and sent
+inline void MsgSetValues(StatusMessage *msg, float left_wheel_vel, float right_wheel_vel)
+{
   msg->linear_velocity = (left_wheel_vel + right_wheel_vel) / 2;
   msg->angular_velocity = (right_wheel_vel - left_wheel_vel) / BASELINE;
 }
 
-void setup() {
+void SysCalibrationLoop(void)
+{
+  Serial.print("Calibrating against a target of ");
+  Serial.print(CALIBRATION_TARGET);
+  Serial.print("ms over ");
+  Serial.print(CALIBRATION_ITERS);
+  Serial.println(" passes...");
+  gPreviousElapsedTimeMs = millis();
+  for (int i = 0; i < CALIBRATION_ITERS; i++)
+  {
+    unsigned long CurrentElapsedTimeMs = millis();
+
+    StatusMessage message = StatusMessage_init_zero;
+
+    pb_ostream_t stream = pb_ostream_from_buffer(pbStreamBuffer, sizeof(pbStreamBuffer));
+
+    MsgSetValues(&message, 0.0, 0.0);
+    message.delta_millis = CurrentElapsedTimeMs - gPreviousElapsedTimeMs;
+    message.status = gFSMCurrentStatus;
+
+    gInducedDelay += message.delta_millis;
+    gPreviousElapsedTimeMs = CurrentElapsedTimeMs;
+
+    if (!pb_encode(&stream, StatusMessage_fields, &message))
+    {
+      Serial.println("Failed to encode");
+      gFSMCurrentStatus = UNKNOWN_ERROR;
+    }
+
+    for (int i = 0; i < stream.bytes_written; i++)
+    {
+      Serial.printf("%02X", pbStreamBuffer[i]);
+    }
+    Serial.println();
+    Serial.flush();
+  }
+  gInducedDelay /= CALIBRATION_ITERS;
+  gInducedDelay -= CALIBRATION_TARGET;
+  if (gInducedDelay > 0)
+  {
+    Serial.print("System cannot reliably send messages each ");
+    Serial.print(CALIBRATION_TARGET);
+    Serial.print("ms, avg error: ");
+    gFSMCurrentStatus = UNKNOWN_ERROR;
+  }
+  else
+  {
+    Serial.print("Calibration OK, introducing a delay of ");
+    gInducedDelay = abs(gInducedDelay) - ENGINEERING_SAFETY_CONSTANT;
+  }
+  Serial.print(gInducedDelay);
+  Serial.println("ms");
+  return;
+}
+
+void setup()
+{
+  pinMode(5, INPUT);
+  pinMode(16, OUTPUT);
   Serial.begin(115200);
   delay(1000);
   Serial.println("OTTO266 READY TO CONQUER!");
   Serial.flush();
-  currentStatus = RUNNING;
+  gFSMCurrentStatus = WAITING_CONFIG;
+  SysCalibrationLoop();
+  Serial.println("Pull D1 up to start transferring data...");
+  Serial.flush();
+  // update elapsed time to avoid overshot deltat on first message
+  gPreviousElapsedTimeMs = millis();
 }
 
-void loop() {
-  delay(20);
-  unsigned long current_tx_millis = millis();
+void loop()
+{
+  delay(gInducedDelay);
+  digitalWrite(16, gIndicatorLedState);
+  if (digitalRead(5))
+  {
+    gIndicatorLedState = LOW;
+    unsigned long current_tx_millis = millis();
 
-  StatusMessage message = StatusMessage_init_zero;
+    StatusMessage message = StatusMessage_init_zero;
 
-  pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    pb_ostream_t stream = pb_ostream_from_buffer(pbStreamBuffer, sizeof(pbStreamBuffer));
 
-  MsgSetVelocities(&message, ssx[curData], sdx[curData]);
-  message.delta_millis = current_tx_millis - previous_tx_millis;
-  message.status = currentStatus;
+    MsgSetValues(&message, gLeftWhlFakeData[gFakeDataCurIdx], gRightWhlFakeData[gFakeDataCurIdx]);
+    message.delta_millis = current_tx_millis - gPreviousElapsedTimeMs;
+    message.status = gFSMCurrentStatus;
 
-  previous_tx_millis = current_tx_millis;
+    gPreviousElapsedTimeMs = current_tx_millis;
 
-  if (!pb_encode(&stream, StatusMessage_fields, &message)) {
-    Serial.println("Failed to encode");
-    currentStatus = UNKNOWN_ERROR;
+    if (!pb_encode(&stream, StatusMessage_fields, &message))
+    {
+      Serial.println("Failed to encode");
+      gFSMCurrentStatus = UNKNOWN_ERROR;
+    }
+
+    for (int i = 0; i < stream.bytes_written; i++)
+    {
+      Serial.printf("%02X", pbStreamBuffer[i]);
+    }
+    Serial.println();
+    Serial.flush();
+    if (++gFakeDataCurIdx >= WHEEL_DATA_LEN)
+      gFakeDataCurIdx = 0;
   }
-
-  for (int i = 0; i < stream.bytes_written; i++) {
-    Serial.printf("%02X", buffer[i]);
+  else
+  {
+    gIndicatorLedState = HIGH;
   }
-
-  Serial.println();
-  Serial.flush();
-  if (++curData >= dataLen) curData = 0;
 }
